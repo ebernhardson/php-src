ebernhardson/php-src

commit cf35dae6e22da6b06d130630abd024e285939a2e

phpdbg next command must stop when leaving function

sapi/phpdbg/phpdbg_prompt.c
@@ -1541,12 +1541,18 @@ ex_is_caught:
 			goto next;
 		}
 
+		/* not while in conditionals */
+		phpdbg_print_opline_ex(execute_data, 0);
+
 		/* perform seek operation */
 		if ((PHPDBG_G(flags) & PHPDBG_SEEK_MASK) && !(PHPDBG_G(flags) & PHPDBG_IN_EVAL)) {
 			/* current address */
 			zend_ulong address = (zend_ulong) execute_data->opline;
 
 			if (PHPDBG_G(seek_ex) != execute_data) {
+				if (PHPDBG_G(flags) & PHPDBG_IS_STEPPING) {
+					goto stepping;
+				}
 				goto next;
 			}
 
@@ -1590,10 +1596,8 @@ ex_is_caught:
 			}
 		}
 
-		/* not while in conditionals */
-		phpdbg_print_opline_ex(execute_data, 0);
-
 		if (PHPDBG_G(flags) & PHPDBG_IS_STEPPING && (PHPDBG_G(flags) & PHPDBG_STEP_OPCODE || execute_data->opline->lineno != PHPDBG_G(last_line))) {
+stepping:
 			PHPDBG_G(flags) &= ~PHPDBG_IS_STEPPING;
 			DO_INTERACTIVE(1);
 		}

sapi/phpdbg/tests/next_001.phpt
@@ -1,0 +1,37 @@
+--TEST--
+Test next command on function boundaries
+--PHPDBG--
+b 4
+r
+n
+
+
+q
+--EXPECTF--
+[Successful compilation of %s]
+prompt> [Breakpoint #0 added at %s:4]
+prompt> [Breakpoint #0 at %s:4, hits: 1]
+>00004: 	echo 0;
+ 00005: }
+ 00006: 
+prompt> 0
+[L5 %s RETURN                  null                                                           %s]
+>00005: }
+ 00006: 
+ 00007: foo();
+prompt> [L8 %s ECHO                    1                                                              %s]
+>00008: echo 1;
+ 00009: 
+prompt> 1
+[L8 %s RETURN                  1                                                              %s]
+[Script ended normally]
+prompt> 
+--FILE--
+<?php
+
+function foo() {
+	echo 0;
+}
+
+foo();
+echo 1;

commit e4a46fe7881a69f98ce24e20d7bc7562f5bce0ab

phpdbg next command must step over function calls
This was broken in cf35dae. The fix is to try to better target the fix
applied in that patch to only instances where we have left the current
context and entered the previous context.

sapi/phpdbg/phpdbg_prompt.c
@@ -1802,16 +1802,16 @@ ex_is_caught:
 			goto next;
 		}
 
-		/* not while in conditionals */
-		phpdbg_print_opline_ex(execute_data, 0);
-
 		/* perform seek operation */
 		if ((PHPDBG_G(flags) & PHPDBG_SEEK_MASK) && !(PHPDBG_G(flags) & PHPDBG_IN_EVAL)) {
 			/* current address */
 			zend_ulong address = (zend_ulong) execute_data->opline;
 
 			if (PHPDBG_G(seek_ex) != execute_data) {
-				if (PHPDBG_G(flags) & PHPDBG_IS_STEPPING) {
+				if (PHPDBG_G(seek_ex)->prev_execute_data &&
+				    phpdbg_user_execute_data(PHPDBG_G(seek_ex)->prev_execute_data) == execute_data &&
+				    PHPDBG_G(flags) & PHPDBG_IS_STEPPING) {
+					phpdbg_print_opline_ex(execute_data, 0);
 					goto stepping;
 				}
 				goto next;
@@ -1856,6 +1856,9 @@ ex_is_caught:
 				}
 			}
 		}
+
+		/* not while in conditionals */
+		phpdbg_print_opline_ex(execute_data, 0);
 
 		if (PHPDBG_G(flags) & PHPDBG_IS_STEPPING && (PHPDBG_G(flags) & PHPDBG_STEP_OPCODE || execute_data->opline->lineno != PHPDBG_G(last_line))) {
 stepping:

sapi/phpdbg/tests/next_002.phpt
@@ -1,0 +1,34 @@
+--TEST--
+Test next command step over functionality
+--SKIPIF--
+<?php
+if (PHP_OS_FAMILY === 'Windows' && ini_get('opcache.jit') && ini_get('opcache.jit_buffer_size')) {
+    die('xfail breakpoint/watchpoint issues with JIT on Windows');
+}
+?>
+--PHPDBG--
+b 7
+r
+n
+q
+--EXPECTF--
+[Successful compilation of %s]
+prompt> [Breakpoint #0 added at %s:7]
+prompt> [Breakpoint #0 at %s:7, hits: 1]
+>00007: foo();
+ 00008: echo 1;
+ 00009: 
+prompt> 0
+[L8       %s ECHO                    1                                                              %s]
+>00008: echo 1;
+ 00009: 
+prompt> 
+--FILE--
+<?php
+
+function foo() {
+	echo 0;
+}
+
+foo();
+echo 1;